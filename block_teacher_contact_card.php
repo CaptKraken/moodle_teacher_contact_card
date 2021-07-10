a378746923a657906a9b82f1a4f5aa2f9d0511f1
Song Kim <kimsong432@gmail.com>
2021-07-11 00:01:58 +0700

    added comments, organized the codes, added khmer strings, added some media queries

--- block_teacher_contact_card.php
+++ block_teacher_contact_card.php
@@ -1,4 +1,3 @@
-
 <?php
 class block_teacher_contact_card extends block_base {
     public function init() {
@@ -15,61 +14,27 @@ class block_teacher_contact_card extends block_base {
         if ($this->content !== null) {
           return $this->content;
         }
+        // importing css style and js script
         echo "<link rel='stylesheet' href='{$CFG->wwwroot}/blocks/teacher_contact_card/styles.css'>
         <script src='{$CFG->wwwroot}/blocks/teacher_contact_card/scripts.js' defer></script>";
 
+        // card color in settings
         $color = get_config('block_teacher_contact_card', 'cardcolor');
         echo "<style>.ti__list--item {
         background-color: {$color};
         }</style>";
 
+        // get strings
         $tag_teacher = get_string('tag_teacher', 'block_teacher_contact_card');
         $tag_assistant = get_string('tag_assistant', 'block_teacher_contact_card');
         $label_email = get_string('label_email', 'block_teacher_contact_card');
         $label_numbers = get_string('label_numbers', 'block_teacher_contact_card');
         $label_message = get_string('label_message', 'block_teacher_contact_card');
-        $msg_no_number = get_string('msg_no_number', 'block_teacher_contact_card');
         $msg_no_teacher = get_string('msg_no_teacher', 'block_teacher_contact_card');
         $msg_no_permission = get_string('msg_no_permission', 'block_teacher_contact_card');
         $msg_not_course_page = get_string('msg_not_course_page', 'block_teacher_contact_card');
 
-     
-        $courseID = $PAGE->course->id;
-
-
-        if ($PAGE->pagelayout === 'course'){
-            echo "<br><br><br>";
-            $current_user_id = $USER->id;
-            $isEnrolled=false;
-            $isAdmin =false;
-            if (!empty($courseID)) {
-                $context  = context_course::instance($courseID);
-                $students = get_enrolled_users($context);
-                
-                foreach ($students as $key => $value) {
-                    echo $value->id."<br>";
-                    if ($value->id === $current_user_id) {
-                        $isEnrolled = true; 
-                        echo "enrolled";
-                        break;
-                    }
-                    else $isEnrolled = false;
-                }    
-                
-                $admins = get_admins();
-                foreach ($admins as $key => $value) {
-                    
-                    if ($value->id === $current_user_id) {
-                        $isAdmin = true;
-                        echo "admin";
-                        break;
-                    }
-                    else $isAdmin = false;
-                }
-            }
-        }
-
-
+        // functions
         /**
          * list all the users of a specific role in a course
          * @param string $role_shortname string of role shortname from mdl_role
@@ -84,67 +49,134 @@ class block_teacher_contact_card extends block_base {
             return get_role_users($role->id, $context);
         };
 
+        /**
+         * returns person's numbers, handle no number, one number, etc.
+         * 
+         * @param int|string|null $person_phone $person_object->phone1;
+         * @param int|string|null $person_mobile $person_object->phone2;
+         * @return string
+         */
+        function validate_numbers($person_phone, $person_mobile)
+        {
+            $msg_no_number = get_string('msg_no_number', 'block_teacher_contact_card');
+            $msg_no_number = $msg_no_number ? $msg_no_number : 'No number on record.';
+
+            // if EITHER phone OR mobile number exists, show.
+            if ($person_phone xor $person_mobile) {
+                $person_numbers = $person_phone ? $person_phone : $person_mobile;
+            }
+            
+            // if both phone AND mobile exist: 
+            if ($person_phone && $person_mobile) {
+
+                // if the phone and mobile numbers are the same, show one.
+                if ($person_phone === $person_mobile) $person_numbers = $person_phone;
+                // if not, show both
+                $person_numbers = "{$person_phone}<br>{$person_mobile}";
+
+            }
+
+            // if NEITHER exists
+            if (empty($person_phone) && empty($person_mobile)) $person_numbers = $msg_no_number;
+                
+            return $person_numbers;
+        }
+
+        //get course id
+        $courseID = $PAGE->course->id;
+
+        // check if admin or enrolled
+        if ($PAGE->pagelayout === 'course'){
+            $current_user_id = $USER->id;
+            $isEnrolled = false;
+            $isAdmin = false;
+            
+            // if there is a course id
+            if (!empty($courseID)) {
+                $context  = context_course::instance($courseID);
+                $students = get_enrolled_users($context);
+                
+                // if enrolled
+                foreach ($students as $key => $value) {
+                    if ($value->id === $current_user_id) {
+                        $isEnrolled = true; 
+                        break;
+                    }
+                    else $isEnrolled = false;
+                }    
+                
+                // if admin
+                $admins = get_admins();
+                foreach ($admins as $key => $value) {
+                    
+                    if ($value->id === $current_user_id) {
+                        $isAdmin = true;
+                        break;
+                    }
+                    else $isAdmin = false;
+                }
+            }
+        }
+
         // getting all teachers from the course
         $teachers = get_list_of_users_by_role('editingteacher', $courseID);
         // getting all teacher assistants from the course
         $teacher_assistants  = get_list_of_users_by_role('teacher', $courseID);
 
         $showAssitants = get_config('block_teacher_contact_card', 'showassistants');
-        // $showAssitants=true;
+        // if show assistants is enabled in settings:
         if ($showAssitants) {
+            // show assistants
             $all_teachers = $teachers + $teacher_assistants;
         } else {
+            // show teachers only
             $all_teachers = $teachers;
         }
 
         $show_email = get_config('block_teacher_contact_card', 'showemail');
         $show_numbers = get_config('block_teacher_contact_card', 'shownumbers');
-        // $show_email = true;
-        // $show_numbers = true;
 
-        $html = '<section class="ti">
-        <ul class="ti__list">';
+
+        $html = '<section class="ti"><ul class="ti__list">';
         $i = 1;
 
         foreach ($all_teachers as $id => $info) {
+            
+            // getting user object
             $person_object = core_user::get_user($id);
             $person_fullname = "$person_object->firstname $person_object->lastname";
             $person_email = $person_object->email;
             $person_phone = $person_object->phone1;
             $person_mobile = $person_object->phone2;
 
-            $person_numbers = '';
-            if ($person_phone xor $person_mobile) {
-                $person_numbers = $person_phone ? $person_phone : $person_mobile;
-            }
-            if ($person_phone && $person_mobile) {
-                if ($person_phone === $person_mobile) $person_numbers = $person_phone;
-                $person_numbers = "{$person_phone}<br>{$person_mobile}";
-            }
-            if (empty($person_phone) && empty($person_mobile)) $person_numbers = $msg_no_number;
-            // if (empty($person_phone) && empty($person_mobile)) $person_numbers = 'no number';
+            $person_numbers = validate_numbers($person_mobile, $person_phone);
 
+            // displaying user picture
             // my moodle discussion thread: https://moodle.org/mod/forum/discuss.php?d=424270
             $conditions = array('size' => '240', 'link' => false, 'class' => '');
             $person_profile_pic =  $OUTPUT->user_picture($person_object, $conditions);
 
-            // if ((int)$info->roleid === 3) $role = 'teacher';
-            // if ((int)$info->roleid === 4) $role = 'teacher assistant';
+            // roleid: 3 = editing teacher, 4 = non-editing teacher (db, mdl_role)
             if ((int)$info->roleid === 3) $role = $tag_teacher;
             if ((int)$info->roleid === 4) $role = $tag_assistant;
 
+            // get message link
             $message_url = $CFG->wwwroot . "/message/index.php?id={$id}";
-            $hidden = $i !== 1 ? 'hide' : '';
 
+            // hidden class if not the first card.
+            $hidden = $i !== 1 ? 'hide' : '';
+            // different collapse icon because the first on isn't hidden
             $colapse_icon = $i === 1 ? "▲" : "▼";
 
+            // show email if show email is enabled in settings
             $email_section = $show_email ? "
             <div>
-                <p>📧 {$label_email}</p>
-                <p>{$person_email}</p>
+            <p>📧 {$label_email}</p>
+            <p>{$person_email}</p>
             </div>"
-                : "";
-
+            : "";
+            
+            // show numbers if show numbers is enabled in settings
             $numbers_section = $show_numbers ? "
             <div>
                 <p>📞 {$label_numbers}</p>
@@ -157,7 +189,7 @@ class block_teacher_contact_card extends block_base {
                     <p class='ti__head'><span>{$person_fullname}</span> <span class='ti__collapse-icon'>{$colapse_icon}</span></p>
                     
                     <article class='ti__card {$hidden}'>  
-                        <div class='ti__card__img' title='view profile'>
+                        <div class='ti__card__img'>
                         {$person_profile_pic}
                         </div>                  
                         <div class='card__content'>
@@ -168,13 +200,13 @@ class block_teacher_contact_card extends block_base {
                             ✉️ <a class='msg-btn' href='{$message_url}'>{$label_message} {$person_fullname}</a>
                             </div>
                         </div>
-                    </article>                
+                    </article>    
+
                 </li>
                 ";
 
             $i += 1;
         }
-        // // 
 
         $html .= '</ul></section>';
 
@@ -182,16 +214,23 @@ class block_teacher_contact_card extends block_base {
         $this->content->text   = $html;
         // $this->content->footer = 'Footer here...';
 
+        // if theres's no teacher, show no teacher message.
         if (count($all_teachers) === 0) $this->content->text = $msg_no_teacher;
-
+        
+        // if public teacher list enable:
         $public_list = get_config('block_teacher_contact_card', 'publiclist');
         if ($public_list) {
+            // show teacher list to the public
             return;
         } else {
+            // if not, dont.
             if (isguestuser()) $this->content->text = $msg_no_permission;
         }
 
+        // if the current page isn't a course page, show not a course page message.
         if ($PAGE->pagelayout !== 'course') $this->content->text = $msg_not_course_page;
+        
+        // if user isn't enrolled and isn't admin, show no permission message.
         if ($isEnrolled===false && $isAdmin===false) $this->content->text = $msg_no_permission;
 
         return $this->content;
---
+++ lang/km/block_teacher_contact_card.php
@@ -0,0 +1,22 @@
+<?php
+$string['pluginname'] = 'កាតព័ត៌មានទំនាក់ទំនងគ្រូ';
+$string['teacher_contact_card'] = 'កាតព័ត៌មានទំនាក់ទំនងគ្រូ';
+$string['teacher_contact_card:addinstance'] = 'បន្ថែមប្លុក កាតព័ត៌មានទំនាក់ទំនងគ្រូ';
+$string['teacher_contact_card:myaddinstance'] = 'បន្ថែមប្លុក កាតព័ត៌មានទំនាក់ទំនងគ្រូ ទៅកាន់ Moodle Page របស់ខ្ញុំ';
+
+$string['setting_showassistants'] = 'បង្ហាយជំនួយគ្រូ';
+$string['setting_publiclist'] = 'បង្ហាញបញ្ជីគ្រូជាសាធារណៈ';
+$string['setting_publiclistdesc'] = 'បង្ហាញបញ្ជីគ្រូជាកាន់សាធារណៈ។​ មានន័យថាសូម្បីតែ guest user ក៏អាចមើលបញ្ជីបានដែរ';
+$string['setting_showemail'] = 'បង្ហាញអ៊ីម៉ែលរបស់គ្រូ';
+$string['setting_shownumbers'] = 'បង្ហាញលេខរបស់គ្រូ';
+$string['setting_cardcolor'] = 'ពណ៌របស់កាត';
+
+$string['tag_teacher'] = 'គ្រូ';
+$string['tag_assistant'] = 'ជំនួយគ្រូ';
+$string['label_email'] = 'អ៊ីម៉ែល';
+$string['label_numbers'] = 'លេខទំនាក់ទំនង';
+$string['label_message'] = 'ផ្ញើសារទៅ';
+$string['msg_no_number'] = 'មិនមានលេខទំនាកទំនងក្នុងកំណត់ត្រាទេ។';
+$string['msg_no_teacher'] = 'មិនមានគ្រូសំរាប់​ Course នេះទេ។';
+$string['msg_no_permission'] = 'អ្នកមិនមានសិទ្ធមើលនូវបញ្ជីគ្រូទេ។';
+$string['msg_not_course_page'] = 'នេះមិនមានជាទំព័រមេរៀនទេ។ Block នេះដើរទៅបានតែពេលនៅលើទំព័រមេរៀនតែប៉ុន្នោះ។';
